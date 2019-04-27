8732d4253b94b980c559b72c034f344fad828a7a
Anthony Fu <anthonyfu117@hotmail.com>
2019-04-27 14:26:18 +0800

    feat: firebase connected!

---
+++ client/plugins/firebase.ts
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app'
+
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const config = {
+  apiKey: 'AIzaSyCGr9QtZjJSsomlM5pTkqiPzeCYr_kQqk4',
+  authDomain: 'splitoast-development.firebaseapp.com',
+  databaseURL: 'https://splitoast-development.firebaseio.com',
+  projectId: 'splitoast-development',
+  storageBucket: 'splitoast-development.appspot.com',
+  messagingSenderId: '918223121466',
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const db = firebase.firestore()
+
+db.settings({ timestampsInSnapshots: true })
