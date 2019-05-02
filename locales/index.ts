76b771db24ab887cf2cdd77e8a6774e49141a987
Anthony Fu <anthonyfu117@hotmail.com>
2019-05-02 19:03:58 +0800

    i18n: add 3 langs

--- locales/index.ts
+++ locales/index.ts
@@ -1,9 +1,15 @@
 import en from './en.json'
 import zhcn from './zh-cn.json'
 import zhtw from './zh-tw.json'
+import fr from './fr.json'
+import jp from './jp.json'
+import ko from './ko.json'
 
 export default {
   en,
+  fr,
+  ko,
+  jp,
   'zh-cn': zhcn,
   'zh-tw': zhtw,
 }
