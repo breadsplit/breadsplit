7b88c3a195d5265a1b052e71cd7c1c1fff500f31
JIN,CHENG-WEI <waynechin4012@gmail.com>
2019-05-20 16:37:05 +0800

    FeedBack middle commit

---
+++ client/components/dialogs/FeedBack.vue
@@ -0,0 +1,19 @@
+<template lang='pug'>
+v-card.feedback
+  app-dialog-bar(@close='close()')
+    | 意見回饋
+  v-container.text-xs-center
+    v-form(ref='form', v-model='valid',lazy-validation)
+    v-textfield()
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class FeedBack extends Vue {
+  close(result?) {
+    this.$emit('close', result)
+  }
+}
+</script>
