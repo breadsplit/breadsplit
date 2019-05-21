476c514ddfa82599b8b8b00505896bd0303ab687
Anthony Fu <anthonyfu117@hotmail.com>
2019-05-21 14:08:18 +0800

    feat: add website bone

---
+++ website/nuxt.config.ts
@@ -0,0 +1,66 @@
+
+import NuxtConfiguration from '@nuxt/config'
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+import pkg from '../package.json'
+
+const debug = process.env.NODE_ENV !== 'production'
+
+const config: NuxtConfiguration = {
+  mode: 'spa',
+  debug,
+
+  head: {
+    title: 'website',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'description', content: pkg.description },
+      { name: 'author', content: pkg.author },
+      { name: 'keywords', content: pkg.keywords.join(',') },
+      { name: 'HandheldFriendly', content: 'true' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    ],
+  },
+  loading: { color: '#3B8070' },
+
+  css: [
+    '~/assets/css/app.styl',
+  ],
+
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~vuetify/src/stylus/settings/_variables.styl'],
+      },
+    },
+    extractCSS: !debug,
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        // @ts-ignore
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue|ts)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+        })
+      }
+    },
+  },
+
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/google-gtag',
+  ],
+
+  'google-gtag': {
+    id: process.env.GOOGLE_GTAG_ID,
+    debug,
+  },
+}
+
+export default config
---
+++ website/pages/index.vue
@@ -0,0 +1,18 @@
+<template lang='pug'>
+section
+  h1.header BreadSplit
+  p This this welcome page of BreadSplit, which is working in progress.
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+}
+</script>
+
+<style lang='stylus'>
+.header
+  font-family "Segoe UI", Tahoma, Geneva, Verdana, sans-serif
+</style>
