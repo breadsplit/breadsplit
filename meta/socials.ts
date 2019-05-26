b29d16cf15e6911e0900e2d21c918e87dae8adb9
Anthony Fu <anthonyfu117@hotmail.com>
2019-05-26 16:57:20 +0800

    chore: update config for new vue-i18n-ally

--- meta/socials.ts
+++ meta/socials.ts
@@ -1,14 +1,17 @@
-/* manage our social links here */
-
 export default {
+  // Original
   homepage: 'https://breadsplit.com',
   github: 'https://github.com/breadsplit/breadsplit',
   github_issue: 'https://github.com/breadsplit/breadsplit/issues',
+  privacy: 'https://www.iubenda.com/privacy-policy/26554021',
+
+  // Donate
   patreon: 'https://www.patreon.com/user?u=19935929',
+  paypal: 'https://www.paypal.me/breadsplit/USD5',
+
+  // Social Media
   instagram: 'https://instagram.com/BreadSplit',
   twitter: 'https://twitter.com/BreadSplit',
-  youtube: 'https://www.youtube.com/channel/UCmEUaFnOtwvSa7jorPf9HDQ',
   medium: 'https://medium.com/breadsplit',
-  paypal: 'https://www.paypal.me/breadsplit/USD5',
-  privacy: 'https://www.iubenda.com/privacy-policy/26554021',
+  youtube: 'https://www.youtube.com/channel/UCmEUaFnOtwvSa7jorPf9HDQ',
 }
