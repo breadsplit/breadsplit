c1c4e9903cad6b9cca7925da0685f0834733990d
Anthony Fu <anthonyfu117@hotmail.com>
2019-05-27 05:05:33 +0800

    chore: add test label

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,5 +1,6 @@
 <template lang='pug'>
 v-container
+  h2 This is a new label
   v-layout(column, justify-center, align-center)
     v-flex.text-xs-center(xs12, sm8, md6)
       template(v-for='(group, i) in groups')
