33f89f3b85bc5723b5d13370816ba885ec5ecfdf
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-09 13:17:36 +0800

    feat: save time zone, close #97

---
+++ client/utils/compat.ts
@@ -0,0 +1,12 @@
+// check if the browser is compatible
+export function isCompat() {
+  try {
+    if (!Intl)
+      return false
+
+    return true
+  }
+  catch (error) {
+    return false
+  }
+}
