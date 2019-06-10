d003273903730715030b6025ee335b5091c73a6f
Anthony Fu <anthonyfu117@hotmail.com>
2019-06-10 18:03:07 +0800

    fix: type error

--- client/plugins/components.ts
+++ client/plugins/components.ts
@@ -6,6 +6,7 @@ import Vue from 'vue'
 const components = require.context('~/components/', true, /(basic|composed|dialogs|global)\/[A-Z]\w+\.(vue)$/)
 components.keys().forEach((fileName) => {
   const componentConfig = components(fileName)
-  const componentName = fileName.split('/').pop().split('.')[0]
-  Vue.component(`App${componentName}`, componentConfig.default || componentConfig)
+  const componentName = (fileName.split('/').pop() || '').split('.')[0] || ''
+  if (componentName)
+    Vue.component(`App${componentName}`, componentConfig.default || componentConfig)
 })
