2c9d34895a86091c7ce0729878a5170fb443c29d
Anthony Fu <anthonyfu117@hotmail.com>
2019-08-23 03:03:13 +0800

    ui: group widget style

---
+++ packages/client/components/composed/GroupWidget.vue
@@ -0,0 +1,91 @@
+<template lang='pug'>
+nuxt-link.group-widget.pa-3(
+  v-ripple
+  :to='`/group/${group.id}`'
+  :style='groupCssVars(group)'
+  v-columns='"max-content auto max-content"'
+)
+  .group-icon
+    v-icon(size='30') mdi-{{ group.icon }}
+  .pa-2
+    .group-name(v-text='group.name')
+    .group-members {{Object.keys(group.members).length}} members
+  .group-status-icon
+    v-icon.op-50(v-if='group.online') mdi-cloud-outline
+</template>
+
+<script lang='ts'>
+import { Component, mixins, Prop } from 'nuxt-property-decorator'
+import { NavigationMixin } from '~/mixins'
+import { Group, ClientGroup } from '~/types'
+
+@Component
+export default class GroupWidget extends mixins(NavigationMixin) {
+  @Prop(String) id!: string
+
+  get group (): Group {
+    return this.$store.getters['group/id'](this.id)
+  }
+
+  get clientGroup (): ClientGroup {
+    return this.$store.getters['group/clientGroupById'](this.id)
+  }
+
+  groupCssVars (group) {
+    return {
+      '--group-color': group.color,
+    }
+  }
+}
+</script>
+
+<style lang='sass'>
+.group-widget
+  --group-color: #000
+  --group-padding: 15px
+
+  height: 85px
+  margin: 10px 5px
+  display: block
+  border-radius: 8px
+  position: relative
+  border: 1px solid rgba(0,0,0,0.1)
+  box-shadow: 1px 1px 5px 0px rgba(50, 50, 50, 0.1)
+  cursor: pointer
+  background: white
+  text-decoration: none
+  color: var(--group-color)
+  text-align: left
+  overflow: hidden
+  font-weight: normal
+
+  .theme--dark &
+    background: #252525
+
+  & > *
+    margin-top: auto
+    margin-bottom: auto
+
+  .group-status-icon
+    padding: 0 0.5em
+
+  .group-icon
+    padding: 0 1em
+
+    .v-icon
+      color: var(--group-color)
+
+  .group-name
+    line-height: 1em
+    white-space: nowrap
+    text-align: left
+    text-overflow: ellipsis
+    overflow: hidden
+    color: var(--group-color)
+
+  .group-members
+    line-height: 1em
+    font-size: 0.8em
+    opacity: 0.5
+    color: rgba(125,125,125)
+</style>
--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -11,12 +11,9 @@ v-container.text-center
         span {{$t('ui.button_new_group')}}
 
   template(v-else)
-    template(v-for='(group, i) in groups')
-      nuxt-link.group-entry(v-ripple, :to='`/group/${group.id}`', :style='groupCssVars(group)')
-        v-icon mdi-{{ group.icon }}
-        .groupname(v-text='group.name')
+    app-group-widget(v-for='(group, i) in groups' :id='group.id' :key='group.id')
 
-    .my-3
+    .my-8
 
     v-btn(@click='gotoNewGroup' rounded color='primary')
       v-icon.mr-2 mdi-plus
@@ -48,47 +45,3 @@ export default class Homepage extends mixins(NavigationMixin) {
   }
 }
 </script>
-
-<style lang='sass'>
-.group-entry
-  --group-color: #000
-  --group-padding: 15px
-
-  width: 140px
-  height: 85px
-  margin: 5px
-  display: inline-block
-  border-radius: 8px
-  position: relative
-  border: 1px solid rgba(0,0,0,0.1)
-  box-shadow: 1px 1px 5px 0px rgba(50, 50, 50, 0.1)
-  cursor: pointer
-  background: white
-
-  .theme--dark &
-    background: #252525
-
-  .v-btn__content
-    display: block
-    opacity: 0.6
-    text-align: center
-
-  .groupname
-    position: absolute
-    left: var(--group-padding)
-    right: var(--group-padding)
-    bottom: var(--group-padding)
-    color: var(--group-color)
-    line-height: 1em
-    white-space: nowrap
-    text-align: left
-    text-overflow: ellipsis
-    overflow: hidden
-
-  .v-icon
-    position: absolute
-    left: var(--group-padding)
-    top: var(--group-padding)
-    color: var(--group-color)
-    font-size: 25px
-</style>
