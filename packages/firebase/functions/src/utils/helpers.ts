7f9bbf69979d48db3b3051b0505c8d240776fdd4
Anthony Fu <anthonyfu117@hotmail.com>
2019-10-22 10:13:02 +0800

    fix: firebase function

--- packages/firebase/functions/src/utils/helpers.ts
+++ packages/firebase/functions/src/utils/helpers.ts
@@ -12,11 +12,11 @@ export const ExchangeRef = (id: string) => db.collection('exchanges').doc(id)
 export const f = functions.https.onCall
 
 export function recalculateGroupOperations (t: FirebaseFirestore.Transaction, groupid: string, ops: Operation[]) {
-  const present = GroupCleanUp(Eval(ops))
+  const present = GroupCleanUp(omitDeep(Eval(ops)))
 
   t.update(OperationsRef(groupid), 'operations', omitDeep(ops))
-  t.update(GroupsRef(groupid), omitDeep({
+  t.update(GroupsRef(groupid), {
     present,
-    'operations': ops.map(o => o.hash),
-  }))
+    operations: ops.map(o => o.hash),
+  })
 }
