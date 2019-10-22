a518ea9b99f150873371e155b4cc14e6a6dfcc0f
Anthony Fu <anthonyfu117@hotmail.com>
2019-10-22 10:07:36 +0800

    chore: cleanup function for firebase

---
+++ packages/firebase/functions/src/utils/cleanup.ts
@@ -0,0 +1,16 @@
+import { Group } from './types'
+
+export function GroupCleanUp (group: Group) {
+  if (!group.exchange_rates)
+    group.exchange_rates = {}
+  for (const transaction of group.transactions) {
+    // @ts-ignore
+    if (transaction.exchange_rate) {
+      // @ts-ignore
+      group.exchange_rates[transaction.exchange_rate.date] = transaction.exchange_rate
+      // @ts-ignore
+      delete transaction.exchange_rate
+    }
+  }
+  return group
+}
--- packages/firebase/functions/src/utils/helpers.ts
+++ packages/firebase/functions/src/utils/helpers.ts
@@ -2,6 +2,7 @@ import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { Eval, omitDeep } from './opschain'
 import { Operation } from './types'
+import { GroupCleanUp } from './cleanup'
 
 export const db = admin.firestore()
 export const GroupsRef = (id: string) => db.collection('groups').doc(id)
@@ -11,7 +12,7 @@ export const ExchangeRef = (id: string) => db.collection('exchanges').doc(id)
 export const f = functions.https.onCall
 
 export function recalculateGroupOperations (t: FirebaseFirestore.Transaction, groupid: string, ops: Operation[]) {
-  const present = Eval(ops)
+  const present = GroupCleanUp(Eval(ops))
 
   t.update(OperationsRef(groupid), 'operations', omitDeep(ops))
   t.update(GroupsRef(groupid), omitDeep({
